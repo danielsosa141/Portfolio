46731486bf606e4af13d93bbd02edc741eca4d76
danielsosa <daniel.sosa-merino@epitech.eu>
2026-04-29 14:58:54 +0200

    page/Home in progress

---
+++ portfolio/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import Hero from "../components/Hero";
+import Projects from "../sections/Projects";
+import About from "../sections/About";
+import Contact from "../sections/Contact";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Projects />
+      <About />
+      <Contact />
+    </>
+  );
+};
+
+export default Home;
